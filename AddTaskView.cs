cad7e6cbcf7a7d2ea9c28eb96ba0669180f6e992
n1k1010 <105913072+n1k1010@users.noreply.github.com>
2022-05-20 10:40:44 +0200

    Add files via upload

---
+++ AddTaskView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo
+{
+    public partial class AddTaskView : Form
+    {
+        public AddTaskView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+           this.Close();
+         
+        }
+    }
+}
---
+++ ToDo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public enum States { ToDo, InProgess, Done }
+
+
+    public class ToDo
+    {
+
+        public string Title { get; private set; }
+        public DateTime DueDate { get; private set; }
+        // suchen nach c# DataGridView Enum to Combobox ( table cell converter wahrscheinlich )
+        public States State { get; private set; } = States.ToDo;
+    }
+
+
+    public void ToDo(string title, DateTime dueDate)
+
+    {
+        Title = title;
+        DueDate = dueDate;
+        
+
+
+    }
+}
+
